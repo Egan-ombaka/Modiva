52975f851e8c2a22b2a75f213f6b1e980242bc82
egan-ombaka <eganombaka97@gmail.com>
2024-10-31 17:32:47 +0300

    modiva

--- client/modiva/src/App.tsx
+++ client/modiva/src/App.tsx
@@ -1,7 +1,12 @@
+import Footer from "./ui/Footer"
+import Header from "./ui/header"
+
 function App() {
   return (
     <main>
+      <Header />
       <p>Modiva e-commerce</p>
+      <Footer />
     </main>
   )
 }
