5be7910b4075fa73d68f354628ee88717079e614
egan-ombaka <eganombaka97@gmail.com>
2024-11-01 15:20:40 +0300

    modiva

--- client/modiva/src/ui/Header.tsx
+++ client/modiva/src/ui/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { logo } from "../assets";
 
 const Header = () => {
-    return <div className="w-full">
-        <div></div>
-    </div>;
-};
-
+    <div className="w-full bg-whiteText">
+        <div className="max-w-screen-xl mx-auto h-20 flex items-center justify-between">
+            <img src={logo} alt="logo" className="w-44"/>
+        </div>
+    </div>
+}
 export default Header;
